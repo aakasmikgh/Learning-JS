360d13f7119b11399363c8fed8836763f317c0c0
Aakasmik Ghimire <akasmikghimire353@gmail.com>
2025-01-11 20:59:09 +0545

    variable rules

---
+++ BASICS/var_rules.js
@@ -0,0 +1,40 @@
+// rules for naming variables
+
+// you cannot start with number
+// example:
+//1value(invalid)
+//value1(valid)
+
+var value1 = 25;
+console.log(value1 * 4);
+
+var value2 = 15;
+console.log(value2 ** 2); //power garna paryo vane duita multiply use garne
+
+
+// you can use only underscore or dollar symbol
+// example: only $ and _
+// first_name (valid)
+// _firstname (valid)
+
+var first$name = "Aakasmik";
+console.log(first$name);
+
+var _firstname = "Ram";
+console.log(_firstname);
+
+//first$name(valid)
+//$firstname(valid)
+
+// you cannot use spaces
+// use underscore instead of that
+
+var first_name = "aakasmik"; //snake case
+var firstName = "aakasmik"; //camel case
+
+
+//convention
+//start with small letter and use camelcase
+
+
+
--- BASICS/variable.js
+++ BASICS/variable.js
@@ -1,3 +1,4 @@
+"use strict"; // kei galti vayo vane bachauxa mostly used by developers.
 // intro to variables
 
 
@@ -6,3 +7,12 @@ var firstName = "Aakasmik" ;
 
 //use a variable
 console.log(firstName);
+
+
+//change a value
+var firstname = "Ram";  // feri var lekhyo vane duita variable banxa yesma value matrai change gareko ho
+console.log(firstname);
+
+console.log(firstName);
+
+
