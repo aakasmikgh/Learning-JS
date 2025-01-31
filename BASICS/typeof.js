919f5c0f0be7b4c6112b3962a61211ac0e069276
Aakasmik Ghimire <akasmikghimire353@gmail.com>
2025-01-31 19:02:23 +0545

    feat:string concatenation

--- BASICS/typeof.js
+++ BASICS/typeof.js
@@ -1,7 +1,41 @@
 // type of operator
 
+let age = 19;
+let firstName = "aakasmik";
+
+
+console.log(typeof age); // typeof stringname le chai type define garxa
+
+console.log(typeof firstName);
+
+console.log(typeof "aakasmik");
+
+console.log(typeof 19);
 
 
 //convert number to string
 
+console.log(typeof (age + "")); // age + "string" converts number to string.
+
+//or
+
+age = age + "";
+console.log(typeof age); // and if we wanna change number to string add "string double cotation"
+
+let Age = "20";
+Age = Number(Age); // or you can change number like this as Number or String
+console.log(typeof Age);
+
+
+
+
+
 //convert string to number
+
+let newStr = "555";
+console.log(typeof newStr);
+
+console.log(typeof (+newStr)); // yedi string lai number ma change garni vaye agadi + add garne
+
+
+                                
